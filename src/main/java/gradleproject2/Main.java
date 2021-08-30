d1a9ff29930a5591b4b7dd56aa3abb298631495e
prasannamundru <durgaprasanna0309@gmail.com>
2021-08-30 11:54:39 -0500

    Java Program
    My First Git Commit

---
+++ src/main/java/gradleproject2/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gradleproject2;
+
+/**
+ *
+ * @author S545022
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        // TODO code application logic here
+    }
+    
+}
